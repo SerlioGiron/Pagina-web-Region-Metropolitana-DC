bbae67066f69cb797fa90046931290a725fc653c
SerlioGiron <serlio.giron@unitec.edu>
2025-02-08 21:29:55 -0600

    probando de una nueva manera para el suspense

--- src/app/Blog-page/page.tsx
+++ src/app/Blog-page/page.tsx
@@ -4,32 +4,17 @@ import {Typography} from "@material-tailwind/react";
 import {useSearchParams} from "next/navigation";
 import {noticias} from "@/data/noticias";
 import { Suspense } from "react";
+import dynamic from "next/dynamic";
+
+const BlogContent = dynamic(() => import("./Blog-content"), { ssr: false });
 
 export default function Blog() {
     const searchParams = useSearchParams();
     const id = searchParams.get("id");
     const noticia = noticias.find((noticia) => noticia.id === Number(id));
     return (
-        <Suspense fallback={<h3>Cargando...</h3>}>
-
-            <div className="justify-center items-center flex flex-col">
-                <StickyNavbar />
-                <Typography
-                    as="h1"
-                    variant="h1"
-                    color="blue-gray"
-                    className="text-center mt-8" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
-                    {noticia.title}
-                </Typography>
-                <img
-                    src={noticia.image}
-                    alt="noticia"
-                    className="w-full h-96 object-cover"
-                />
-                <Typography variant="paragraph" color="gray" className="mt-8 px-[16%] text-justify justify-center" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-                    {noticia.content}
-                </Typography>
-            </div>
+        <Suspense fallback={<div>Loading...</div>}>
+            <BlogContent/>
         </Suspense>
     );
 }
