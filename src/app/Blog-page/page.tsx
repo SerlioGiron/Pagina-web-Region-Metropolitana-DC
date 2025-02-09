01fe860928eab15681e8f26a7c14ecc9e6ad42ca
SerlioGiron <serlio.giron@unitec.edu>
2025-02-09 12:17:46 -0600

    probando una vez mas

--- src/app/Blog-page/page.tsx
+++ src/app/Blog-page/page.tsx
@@ -3,12 +3,12 @@ import {StickyNavbar} from "@/components/StickyNavbar";
 import {Typography} from "@material-tailwind/react";
 import {useSearchParams} from "next/navigation";
 import {noticias} from "@/data/noticias";
-import { Suspense } from "react";
+import {Suspense} from "react";
 import dynamic from "next/dynamic";
 
 //const BlogContent = dynamic(() => import("./Blog-content"), { ssr: false });
 
-export default function Blog() {
+function BlogContent() {
     const searchParams = useSearchParams();
     const id = searchParams.get("id");
     const noticia = noticias.find((noticia) => noticia.id === Number(id));
@@ -19,7 +19,11 @@ export default function Blog() {
                 as="h1"
                 variant="h1"
                 color="blue-gray"
-                className="text-center mt-8" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}            >
+                className="text-center mt-8"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+            >
                 {noticia.title}
             </Typography>
             <img
@@ -27,9 +31,29 @@ export default function Blog() {
                 alt="noticia"
                 className="w-full h-96 object-cover"
             />
-            <Typography variant="paragraph" color="gray" className="mt-8 px-[16%] text-justify justify-center" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
+            <Typography
+                variant="paragraph"
+                color="gray"
+                className="mt-8 px-[16%] text-justify justify-center"
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+            >
                 {noticia.content}
             </Typography>
+        </div>
+    );
+}
+
+export default function Blog() {
+    return (
+        <div className="justify-center items-center flex flex-col">
+            <StickyNavbar />
+            <Suspense
+                fallback={<h2>Cargando...</h2>}
+            >
+                <BlogContent />
+            </Suspense>
         </div>
     );
 }
