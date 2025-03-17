1f3a33bed79876dbaf1348bb843e33e0cbf60e97
SerlioGiron <serlio.giron@unitec.edu>
2025-03-17 13:20:20 -0600

    cambio en el video de yt, loop

--- src/components/YouTubeShort.jsx
+++ src/components/YouTubeShort.jsx
@@ -5,7 +5,7 @@ export function YouTubeShort ({ videoId })  {
         <div className="flex justify-center items-center p-4">
           <iframe
             className="w-full max-w-4xl my-10 h-[500px] rounded-lg shadow-lg"
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1`}
+            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}`}
             title="YouTube Video"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
