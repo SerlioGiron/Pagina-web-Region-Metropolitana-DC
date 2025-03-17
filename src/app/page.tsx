fd455c1aa30d78b4d8b0ea8fbbc299569ef95c15
SerlioGiron <serlio.giron@unitec.edu>
2025-03-17 14:59:06 -0600

    trabajo por hoy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import {YouTubeShortCarousel} from "../components/YouTubeShortsCarousel";
 
 export default function Home() {
     return (
-        <div className="max-h-[768px]">
+        <div className="max-h-[600px]">
             <StickyNavbar />
             <CarouselDefault />
             <YouTubeShort videoId="IUj3Hl0QTQ8" />
--- src/components/YouTubeShort.jsx
+++ src/components/YouTubeShort.jsx
@@ -4,7 +4,7 @@ export function YouTubeShort ({ videoId })  {
     return (
       <div className="flex justify-center items-center p-4 bg-[#68c5d7]">
         <iframe
-        className="w-full max-w-4xl my-10 h-[500px] rounded-lg shadow-lg"
+        className="w-full max-w-4xl my-10 h-[80vh] rounded-lg shadow-lg"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}`}
         title="YouTube Video"
         frameBorder="0"
