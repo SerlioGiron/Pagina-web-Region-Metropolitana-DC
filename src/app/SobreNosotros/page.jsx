9abc1de74fd4971cd33223ddae3673cfe9b6f4a0
SerlioGiron <serlio.giron@unitec.edu>
2025-03-24 12:14:39 -0600

    se agrego info en mision y vision

--- src/app/SobreNosotros/page.jsx
+++ src/app/SobreNosotros/page.jsx
@@ -22,9 +22,20 @@ const Mision = () => {
                     Misión
                 </Typography>
                 <Typography className="text-gray-600">
-                    Nuestra misión es proporcionar servicios de salud accesibles
-                    y de calidad, promoviendo el bienestar y la prevención de
-                    enfermedades en nuestra comunidad.
+                    Somos la Región Sanitaria Metropolitana del Distrito
+                    Central, responsable de ejecutar, controlar, dar seguimiento
+                    y evaluar según normas, planes y programas nacionales de
+                    salud: ejerciendo la rectoría de todos los Establecimientos
+                    proveedores de servicios de Salud dentro del Distrito
+                    Central; dedicada a orientar, promover, conducir y regular
+                    la construcción de entornos saludables haciendo un uso
+                    eficiente de los recursos asignados, garantizando la
+                    seguridad, calidad de bienes, servicios de interés sanitario
+                    y la intervención sobre riesgos y daños colectivos a la
+                    Salud, ejecutando las políticas en atención a las
+                    necesidades y demandas de salud con un enfoque prioritario
+                    en fortalecer el Primer Nivel de Atención con coordinaciones
+                    interinstitucional.
                 </Typography>
             </CardBody>
         </Card>
@@ -45,9 +56,16 @@ const Vision = () => {
                     Visión
                 </Typography>
                 <Typography className="text-gray-600">
-                    Ser una institución líder en el sector salud, reconocida por
-                    su excelencia en la atención médica y su compromiso con la
-                    innovación y la equidad.
+                    Ser una Región Sanitaria que garantiza las condiciones
+                    favorables para la Salud de la población que habita en el
+                    Municipio del Distrito Central, mediante la construcción
+                    colectiva y la conducción apropiada de un Sistema de Salud
+                    plural e integrado debidamente regulado, centrado en la
+                    atención primaria en salud, abordando de forma integral a la
+                    persona, familia y comunidad, con enfoque participativo e
+                    intersectorial, equidad, acceso universal en la cobertura,
+                    calidez, oportunidad, eficacia y eficiencia en la prestación
+                    de servicios con altos niveles de calidad.
                 </Typography>
             </CardBody>
         </Card>
